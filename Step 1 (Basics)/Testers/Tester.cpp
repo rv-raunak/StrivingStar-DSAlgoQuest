84c019e37e102682b3c641d70cacb4839c2729a2
rv-raunak <rvr.vaibhavmishra@gmail.com>
2025-08-08 17:12:38 +0530

    chore: restore tester defaults

--- Step 1 (Basics)/Testers/Tester.cpp
+++ Step 1 (Basics)/Testers/Tester.cpp
@@ -1,28 +1,9 @@
 #include<iostream>
 using namespace std;
 
+
 int main()
 {
-    string s;
-    cin>>s;
-
-    //pre-compute
-    int hash[26] = {0};
-    for(int i  = 0; i<s.size(); i++)
-    {
-        hash[s[i]-'a']++;
-    }
-
-
-    //fetching 
-    int q;   //query size
-    cin>>q;
-    while(q--)
-    {
-        char c;
-        cin>>c;
-
-        cout<<hash[c-'a']<<endl;
-    }
+    
     return 0;
 }
